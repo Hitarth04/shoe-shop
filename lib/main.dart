beb614efeed1f661f8a3c438d4dc92434b0c20cf
hitarth04 <davehitarth04@gmail.com>
2026-01-08 12:37:46 +0530

    `Added settings and payment methods management to ProfileScreen`

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Roboto',
         useMaterial3: true,
       ),
-      home: const OnboardingScreen(),
+      home: OnboardingScreen(),
     );
   }
 }
