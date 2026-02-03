badaaa911dbfd3588c7de4bffe862145e8d6bed2
hitarth04 <davehitarth04@gmail.com>
2026-02-03 12:37:35 +0530

    Added notification and order filter features

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,14 +16,14 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
+    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID
+        // (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.shoe_shop"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
@@ -42,6 +42,9 @@ android {
     }
 }
 
-flutter {
-    source = "../.."
+flutter { source = "../.." }
+
+dependencies {
+    // This is correct, keep it here.
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
